a26a520458d16fbccb2576d39a1d775a1191d86f
Okechukwu Chukwude <chukwudeokechukwu@gmail.com>
2023-05-26 18:12:15 +0100

    Remove comments

--- scrapper.py
+++ scrapper.py
@@ -7,7 +7,6 @@ from langchain.agents import initialize_agent, Tool
 from langchain.utilities import GoogleSerperAPIWrapper
 
 gpt3 = OpenAI(model_name='text-davinci-003', temperature =0) #OpenAI LLM with a temperature of 0 increasing its creativity/ randomness
-# ollm = OpenAI(temperature=0.7) 
 
 #The search tool used to search the internet
 tool_names = ["serpapi"]
@@ -31,18 +30,15 @@ agent2 = initialize_agent(tools2, llm =gpt3 , agent="zero-shot-react-description
 
 def scrape(company_name, country, url=None):
     ''' 
-    Function that takes in Company and country, 
-    and searches the internet to get's the description and then gets 
-    the products and services from the description as a commaseperated list. 
+    Function that takes in Company name and country, 
+    and searches the internet to get company's description,
+    products/services and SIC & NAIC Codes. 
 
     Keyword arguments:
     company_name: str 
     country: str 
-
-    Returns 
-    (Description : str, products :str) : tuple
-       
     '''
+    
     try:
         query = f'''
             I need you to answer some questions about the company in double  quotes ''{company_name}'' that is based in this country in angle brackets <{country}> :
@@ -69,37 +65,10 @@ def scrape(company_name, country, url=None):
         details = json.loads(details)
 
         prod_s_codes = agent2.run(f'give me only the SIC code without any text for the products in this list {details["Products"]}  ')
-        prod_n_codes = agent2.run(f'give me only the NAICS code without any text for the products in this list {details["Products"]}  ')
-
-        
-        # query2 = f'''
-        # I need you to answer some questions about the values in the list in the double quotes ''{details["Products"]}'' : 
-
-        # 1 - What are the SIC codes from SEC for all the values in the list 
-        # 2 - What are the NAICS codes for all the values in the list
-
-        # Return a python dictionary object with the following keys: SIC Products, NAICS Products, where all the values are in a list and not a string object.
-        # Make sure to use double quotes for the keys of the JSON object
-        # Make sure to find the codes for each value seperately.
-        # Make sure you only return SIC and NAICS Codes in the Python dictionary, there should be no text in the values output
-        # '''
-
-        # prod_s_n_codes = agent.run(query2)
-
-        # query3 = f'''
-        # I need you to answer some questions about the values in the list in the double quotes ''{details["Services"]}'' : 
-
-        # 1 - What are the SIC codes from SEC for all the values in the list 
-        # 2 - What are the NAICS codes for all the values in the list
-
-        # Return a python dictionary object with the following keys: SIC Service, NAICS Services, where all the values are in a list and not a string object.
-        # Make sure to use double quotes for the keys of the JSON object
-        # Make sure to find the codes for each value seperately.
-        # Make sure you only return SIC and NAICS Codes in the Python dictionary, there should be no text in the values output
-        # '''
+        prod_n_codes = agent2.run(f'give me only the NAIC code without any text for the products in this list {details["Products"]}  ')
 
         serv_s_codes = agent2.run(f'give me only the SIC code without any text for the services in this list {details["Services"]}')
-        serv_n_codes = agent2.run(f'give me only the NAICS code without any text for the services in this list {details["Services"]}')
+        serv_n_codes = agent2.run(f'give me only the NAIC code without any text for the services in this list {details["Services"]}')
 
         return (details, prod_s_codes, prod_n_codes, serv_n_codes, serv_s_codes)
         
@@ -107,7 +76,3 @@ def scrape(company_name, country, url=None):
         print(e)
         return 'There was an error. Try again!'
 
-
-if __name__ == '__main__':
-    answer = agent.run(f"What products/services does the company Google offer?")
-    print(answer)
